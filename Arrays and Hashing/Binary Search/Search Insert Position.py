64245f38ed68ec62255f0aed6f76dec090ec0694
HarjotIris <harjotsingh22nov@gmail.com>
2025-08-18 12:29:24 +0530

    Solved easy - Arrays

--- Arrays and Hashing/Binary Search/Search Insert Position.py
+++ Arrays and Hashing/Binary Search/Search Insert Position.py
@@ -1,5 +1,6 @@
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
+        '''
         low, high = 0, len(nums) - 1
 
         while low <= high:
@@ -11,6 +12,22 @@ class Solution:
             else:
                 return mid
             
+        return low
+        '''
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low+high)//2
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] < target:
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
         return low
     
 sol = Solution()
